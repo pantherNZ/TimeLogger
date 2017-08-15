592e706f7fd343bb25a941b485a7b5c5f64fdce9
pantherNZ <pantherNZ@users.noreply.github.com>
2017-08-15 17:31:50 +1200

    Added some settings to settings form

--- TimeLogger/TimeLogger/SettingsForm.Designer.cs
+++ TimeLogger/TimeLogger/SettingsForm.Designer.cs
@@ -30,12 +30,19 @@
         {
             this.Accept_Button = new System.Windows.Forms.Button();
             this.Cancel_Button = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
-            this.checkBox4 = new System.Windows.Forms.CheckBox();
-            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.RunOnStartup_Checkbox = new System.Windows.Forms.CheckBox();
+            this.ExportToTextFile_Checkbox = new System.Windows.Forms.CheckBox();
+            this.SaveLocalCopy_Checkbox = new System.Windows.Forms.CheckBox();
+            this.AlwaysOnTop_Checkbox = new System.Windows.Forms.CheckBox();
+            this.OpenToMiniView_Checkbox = new System.Windows.Forms.CheckBox();
             this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // Accept_Button
@@ -68,77 +75,147 @@
             this.Cancel_Button.Text = "Cancel";
             this.Cancel_Button.UseVisualStyleBackColor = true;
             // 
-            // checkBox1
+            // RunOnStartup_Checkbox
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(24, 39);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(96, 17);
-            this.checkBox1.TabIndex = 4;
-            this.checkBox1.Text = "Run on startup";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.RunOnStartup_Checkbox.AutoSize = true;
+            this.RunOnStartup_Checkbox.Location = new System.Drawing.Point(167, 8);
+            this.RunOnStartup_Checkbox.Name = "RunOnStartup_Checkbox";
+            this.RunOnStartup_Checkbox.Size = new System.Drawing.Size(96, 17);
+            this.RunOnStartup_Checkbox.TabIndex = 4;
+            this.RunOnStartup_Checkbox.Text = "Run on startup";
+            this.RunOnStartup_Checkbox.UseVisualStyleBackColor = true;
             // 
-            // checkBox2
+            // ExportToTextFile_Checkbox
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(24, 58);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(104, 17);
-            this.checkBox2.TabIndex = 5;
-            this.checkBox2.Text = "Export to text file";
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.ExportToTextFile_Checkbox.AutoSize = true;
+            this.ExportToTextFile_Checkbox.Location = new System.Drawing.Point(167, 27);
+            this.ExportToTextFile_Checkbox.Name = "ExportToTextFile_Checkbox";
+            this.ExportToTextFile_Checkbox.Size = new System.Drawing.Size(104, 17);
+            this.ExportToTextFile_Checkbox.TabIndex = 5;
+            this.ExportToTextFile_Checkbox.Text = "Export to text file";
+            this.ExportToTextFile_Checkbox.UseVisualStyleBackColor = true;
             // 
-            // checkBox3
+            // SaveLocalCopy_Checkbox
             // 
-            this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(24, 76);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(102, 17);
-            this.checkBox3.TabIndex = 6;
-            this.checkBox3.Text = "Save local copy";
-            this.checkBox3.UseVisualStyleBackColor = true;
+            this.SaveLocalCopy_Checkbox.AutoSize = true;
+            this.SaveLocalCopy_Checkbox.Location = new System.Drawing.Point(10, 8);
+            this.SaveLocalCopy_Checkbox.Name = "SaveLocalCopy_Checkbox";
+            this.SaveLocalCopy_Checkbox.Size = new System.Drawing.Size(102, 17);
+            this.SaveLocalCopy_Checkbox.TabIndex = 6;
+            this.SaveLocalCopy_Checkbox.Text = "Save local copy";
+            this.SaveLocalCopy_Checkbox.UseVisualStyleBackColor = true;
             // 
-            // checkBox4
+            // AlwaysOnTop_Checkbox
             // 
-            this.checkBox4.AutoSize = true;
-            this.checkBox4.Location = new System.Drawing.Point(24, 95);
-            this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(92, 17);
-            this.checkBox4.TabIndex = 7;
-            this.checkBox4.Text = "Always on top";
-            this.checkBox4.UseVisualStyleBackColor = true;
+            this.AlwaysOnTop_Checkbox.AutoSize = true;
+            this.AlwaysOnTop_Checkbox.Location = new System.Drawing.Point(10, 27);
+            this.AlwaysOnTop_Checkbox.Name = "AlwaysOnTop_Checkbox";
+            this.AlwaysOnTop_Checkbox.Size = new System.Drawing.Size(92, 17);
+            this.AlwaysOnTop_Checkbox.TabIndex = 7;
+            this.AlwaysOnTop_Checkbox.Text = "Always on top";
+            this.AlwaysOnTop_Checkbox.UseVisualStyleBackColor = true;
             // 
-            // checkBox5
+            // OpenToMiniView_Checkbox
             // 
-            this.checkBox5.AutoSize = true;
-            this.checkBox5.Location = new System.Drawing.Point(24, 113);
-            this.checkBox5.Name = "checkBox5";
-            this.checkBox5.Size = new System.Drawing.Size(110, 17);
-            this.checkBox5.TabIndex = 8;
-            this.checkBox5.Text = "Open to mini view";
-            this.checkBox5.UseVisualStyleBackColor = true;
+            this.OpenToMiniView_Checkbox.AutoSize = true;
+            this.OpenToMiniView_Checkbox.Location = new System.Drawing.Point(10, 45);
+            this.OpenToMiniView_Checkbox.Name = "OpenToMiniView_Checkbox";
+            this.OpenToMiniView_Checkbox.Size = new System.Drawing.Size(110, 17);
+            this.OpenToMiniView_Checkbox.TabIndex = 8;
+            this.OpenToMiniView_Checkbox.Text = "Open to mini view";
+            this.OpenToMiniView_Checkbox.UseVisualStyleBackColor = true;
             // 
             // domainUpDown1
             // 
-            this.domainUpDown1.Location = new System.Drawing.Point(163, 36);
+            this.domainUpDown1.Location = new System.Drawing.Point(167, 47);
             this.domainUpDown1.Name = "domainUpDown1";
             this.domainUpDown1.Size = new System.Drawing.Size(45, 20);
             this.domainUpDown1.TabIndex = 9;
             this.domainUpDown1.Text = "30";
+            this.domainUpDown1.SelectedItemChanged += new System.EventHandler(this.domainUpDown1_SelectedItemChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(214, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Max paid break (mins)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(10, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(208, 20);
+            this.textBox1.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(224, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(25, 20);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(249, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Output directory";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(10, 100);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(208, 20);
+            this.textBox2.TabIndex = 14;
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(224, 99);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(25, 20);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(250, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Output file name";
             // 
             // SettingsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(342, 233);
+            this.ClientSize = new System.Drawing.Size(338, 229);
             this.ControlBox = false;
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.domainUpDown1);
-            this.Controls.Add(this.checkBox5);
-            this.Controls.Add(this.checkBox4);
-            this.Controls.Add(this.checkBox3);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.OpenToMiniView_Checkbox);
+            this.Controls.Add(this.AlwaysOnTop_Checkbox);
+            this.Controls.Add(this.SaveLocalCopy_Checkbox);
+            this.Controls.Add(this.ExportToTextFile_Checkbox);
+            this.Controls.Add(this.RunOnStartup_Checkbox);
             this.Controls.Add(this.Cancel_Button);
             this.Controls.Add(this.Accept_Button);
             this.ForeColor = System.Drawing.SystemColors.ButtonFace;
@@ -158,11 +235,18 @@
 
         private System.Windows.Forms.Button Accept_Button;
         private System.Windows.Forms.Button Cancel_Button;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.CheckBox checkBox3;
-        private System.Windows.Forms.CheckBox checkBox4;
-        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox RunOnStartup_Checkbox;
+        private System.Windows.Forms.CheckBox ExportToTextFile_Checkbox;
+        private System.Windows.Forms.CheckBox SaveLocalCopy_Checkbox;
+        private System.Windows.Forms.CheckBox AlwaysOnTop_Checkbox;
+        private System.Windows.Forms.CheckBox OpenToMiniView_Checkbox;
         private System.Windows.Forms.DomainUpDown domainUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
     }
 }
--- TimeLogger/TimeLogger/SettingsForm.cs
+++ TimeLogger/TimeLogger/SettingsForm.cs
@@ -16,5 +16,15 @@ namespace TimeLogger
         {
             InitializeComponent();
         }
+
+        private void label1_Click( object sender, EventArgs e )
+        {
+
+        }
+
+        private void domainUpDown1_SelectedItemChanged( object sender, EventArgs e )
+        {
+
+        }
     }
 }
